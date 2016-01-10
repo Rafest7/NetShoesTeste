6b29cfad9513765e77c54bef2a85ea57076f7d65
Raphael Ferreira dos Santos <raphaelferreiradossantos@iMac-de-Raphael.local>
2016-01-10 13:53:41 -0200

    add * dos arquivos do commit anterior

---
+++ NetShoesTeste/ProductCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductCollectionViewCell.h
+//  NetShoesTeste
+//
+//  Created by Raphael Ferreira dos Santos on 10/01/16.
+//  Copyright © 2016 Raphael Ferreira dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ NetShoesTeste/ProductCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.m
+//  NetShoesTeste
+//
+//  Created by Raphael Ferreira dos Santos on 10/01/16.
+//  Copyright © 2016 Raphael Ferreira dos Santos. All rights reserved.
+//
+
+#import "ProductCollectionViewCell.h"
+
+@implementation ProductCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
