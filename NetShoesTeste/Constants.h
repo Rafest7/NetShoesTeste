c0b851f6683292091d306e563492f074b4aa3d90
Raphael Ferreira dos Santos <raphaelferreiradossantos@iMac-de-Raphael.local>
2016-01-14 00:10:56 -0200

    Adicionado arquivo com constantes e storyboard da segunda tela

---
+++ NetShoesTeste/Constants.h
@@ -0,0 +1,20 @@
+//
+//  Constants.h
+//  NetShoesTeste
+//
+//  Created by Raphael Ferreira dos Santos on 14/01/16.
+//  Copyright © 2016 Raphael Ferreira dos Santos. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#endif /* Constants_h */
+
+// iPhones
+#define screen_size_iPhone_4s [[UIScreen mainScreen] bounds].size.height == 480
+#define screen_size_iPhone_5_5s [[UIScreen mainScreen] bounds].size.height == 568
+#define screen_size_iPhone_6 [[UIScreen mainScreen] bounds].size.height == 667
+#define screen_size_iPhone_6Plus [[UIScreen mainScreen] bounds].size.height == 736
+// iPad
+#define screen_size_iPad [[UIScreen mainScreen] bounds].size.height == 1024
